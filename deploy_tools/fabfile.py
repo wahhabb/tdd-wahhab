610efbefa0562ec8a36cc2bf9f32c2938f484af8
WAHHAB BALDWIN <wahhabb@gmail.com>
2015-10-30 15:03:01 -1000

    fix deploy-tools

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https.//github.com/wahhab/tdd-wahhab.git'
+REPO_URL = 'https://github.com/wahhabb/tdd-wahhab.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)
@@ -28,19 +28,19 @@ def _get_latest_source(source_folder):
     
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
-    sed(settings_path, "DEBUG = True", "DEBUT = False")
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path, 'ALLOWED_HOSTS = .+%', 
-                        'ALLOWED_HOSTS = ["%S"]' % (site_name,)
+                        'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     ) 
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50)) 
-        append(secret_key_file, "SECRET_KEY = '%S'" % (KEY,))  
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))  
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
     
 def _update_virtualenv(source_folder):
-    virtualenv_folder = source_folder + '/..virtualenv'
+    virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
     run('%s/bin/pip install -r %s/requirements.txt' % (
