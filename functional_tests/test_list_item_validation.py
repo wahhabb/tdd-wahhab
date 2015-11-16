4010a253aa009c3d80eefe67c3b28e5c3aecc72d
WAHHAB BALDWIN <wahhabb@gmail.com>
2015-11-16 12:21:59 -0700

    Javascript to clear error messages

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -48,7 +48,7 @@ class ItemValidationTest(FunctionalTest):
         error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
         
-    def test_error_messages_are_clearned_on_input(self):
+    def test_error_messages_are_cleared_on_input(self):
         #Edith starts a new list in a way that causes a validation error:
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
